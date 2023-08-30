343d89c27be0b5d94a575ac2ce7efe996798c40b
alexssandro772 <alex.araujo77@gmail.com>
2023-08-30 09:36:06 -0300

    Projeto Pronto

--- DesafiosDeCodigo/src/desafio1.java
+++ DesafiosDeCodigo/src/desafio1.java
@@ -6,12 +6,12 @@ public class desafio1 {
             System.out.println("Equilibrando Saldo");
             Scanner scanner = new Scanner(System.in);
     
-            double saldoAtual = scanner.nextDouble();
-            double valorDeposito = scanner.nextDouble();
-            double valorRetirada = scanner.nextDouble();
+            // double saldoAtual = scanner.nextDouble();
+            // double valorDeposito = scanner.nextDouble();
+            // double valorRetirada = scanner.nextDouble();
     
            //TODO: Calcular o saldo atualizado de acordo com a descrição deste desafio.
-           
+
     
             //TODO: Imprimir o a saída de conforme a tabela de exemplos (uma casa decimal).
 
